e8b281666503802026eef6490f693dead00e3ff0
MreH66 <gvozdenovmarko@gmail.com>
2026-06-26 16:27:48 +0200

    fixed component issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default async function Home() {
           }}
           movies={movies}
         />
-        <UpcomingList />
+        <UpcomingList headerType={"main"} />
       </div>
     </div>
   );
